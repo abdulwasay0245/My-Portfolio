6528a28ec307c2ae3f66eb1326c390acc6e13af7
Abdul Wasay <abdulwasay0245@gmail.com>
2025-05-21 13:24:03 +0500

    added skills grid

--- src/app/component/skills.tsx
+++ src/app/component/skills.tsx
@@ -8,17 +8,21 @@ const Skills = () => {
         
         <div className='py-6 space-y-2'>
         <h1 className={`${scriptFont.className} text-6xl text-orange`}>Skills</h1>
-            <div className='grid grid-cols-3 grid-rows-3 border-white border-2 h-screen w-full'></div>
+            <div className='grid grid-cols-3 border-white border-2 h-screen w-full'>
             
             {skills.map((skill) => (
-                <Image key={math.random()}
+                <div key={math.random()}>
+
+                <Image
                     alt='s'
-                    width={500}
-                    height={500}
+                    width={100}
+                    height={100}
                     src={skill.img}>
 
                 </Image>
+                        </div>
             ))}
+                </div>
         </div>
         
 
